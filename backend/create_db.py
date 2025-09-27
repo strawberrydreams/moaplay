8a37dfcb802e2b17522fcecef7be4c1c5c6d7f3a
mui <jsg0227@naver.com>
2025-09-28 08:18:49 +0900

    refac

--- backend/create_db.py
+++ backend/create_db.py
@@ -4,13 +4,7 @@ from app.models import db
 app = create_app()
 
 with app.app_context():
-    from app.models.user import User
-    from app.models.event import Event
-    from app.models.review import Review
-    from app.models.schedule import Schedule
-    from app.models.favorite import Favorite
-    from app.models.tag import Tag
-    from app.models.event_tag import EventTag
 
+    db.drop_all()
     db.create_all()
     print("데이터베이스 테이블이 생성되었습니다.")
